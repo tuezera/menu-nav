8ee9e7e4dc0498b34a1b547372a2c90df2d16faf
Matheus Barbosa <ps.matheusb@gmail.com>
2022-03-27 23:29:50 -0300

    correções no JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,12 @@
-const menu_toggle = document.querySelector('menu_toggle');
-const navigation = document.querySelector('navigation');
+const menu_toggle = document.querySelector('.menu_toggle');
+const navigation = document.querySelector('.navigation');
+
+menu_toggle.addEventListener('click', () =>{
+    navigation.classList.toggle('ativo');
+});
+
+
+/*menu_toggle.onclick = function(){
+    navigation.classList.toggle('ativo');
+}*/
+
